ed5cb29748f68c90ad20d7db320be3cef4818d1a
nielsperdon <niels.perdon@gmail.com>
2022-11-28 18:22:53 +0100

    adding Binary search

--- src/Theatre.java
+++ src/Theatre.java
@@ -7,9 +7,9 @@ public class Theatre {
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
-        int lastRow = 'A' + (numRows -1);
-        for (char row = 'A'; row <= lastRow; row++){
-            for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++){
+        int lastRow = 'A' + (numRows - 1);
+        for (char row = 'A'; row <= lastRow; row++) {
+            for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
                 Seat seat = new Seat(row + String.format("%02d", seatNum));
                 seats.add(seat);
             }
@@ -20,29 +20,27 @@ public class Theatre {
         return theatreName;
     }
 
-    public boolean reserveSeat(String seatNumber){
-        Seat requestedSeat = null;
-        for (Seat seat : seats){
-            if (seat.getSeatNumber().equals(seatNumber)){
-                requestedSeat = seat;
-                break;
-            }
-        }
-        if (requestedSeat == null){
-            System.out.println("There is no seat " + seatNumber);
+    // Binary search is the fastest way to find an item in a sorted List
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
+            System.out.println("there is no seat " + seatNumber);
             return false;
         }
-        return requestedSeat.reserve();
+
     }
 
     // for testing
-    public void getseats(){
-        for (Seat seat  : seats){
+    public void getseats() {
+        for (Seat seat : seats) {
             System.out.println(seat.getSeatNumber());
         }
     }
 
-    private static class Seat{
+    private static class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
@@ -50,8 +48,13 @@ public class Theatre {
             this.seatNumber = seatNumber;
         }
 
-        public boolean reserve(){
-            if (!this.reserved){
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
+        }
+
+        public boolean reserve() {
+            if (!this.reserved) {
                 this.reserved = true;
                 System.out.println("Seat " + seatNumber + " reserved");
                 return true;
@@ -59,11 +62,11 @@ public class Theatre {
             return false;
         }
 
-        public boolean cancel(){
-            if (this.reserved){
-                this.reserved =false;
+        public boolean cancel() {
+            if (this.reserved) {
+                this.reserved = false;
                 System.out.println("Reservation of seat " + seatNumber + " Canceled");
-            return true;
+                return true;
             }
             return false;
         }
